503713fb414b56dc9fc76843abdc9244758ab265
Arpana-Kumari1307 <arpanakumari1307@gmail.com>
2026-02-14 13:06:51 +0530

    Refactor URL patterns and add redirect to app

--- toogle/toogle/urls.py
+++ toogle/toogle/urls.py
@@ -19,10 +19,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.urls import views as auth_views
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/app/')),
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+
